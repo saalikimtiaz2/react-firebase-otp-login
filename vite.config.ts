bd5b2afbe2c0245f64885c8ae516910d42d34f9d
Salik Imtiaz <r.salikimtiaz@gmail.com>
2023-05-18 16:25:58 +0400

    dev mode added to vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,8 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh(), tsconfigPaths()],
+  mode: 'development',
+  build: {
+    minify: false,
+  },
 });
